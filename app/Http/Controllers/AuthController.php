4b8d160ab4ed3faed1f61cd504f421d31735932b
Michael Michael <hallo@michaelfromage.com>
2024-11-20 22:38:40 +0100

    Update auth response

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,8 +19,13 @@ class AuthController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-
-        return response()->json(['message' => 'User registered successfully!'], 201);
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'user' => $user,
+            'message' => 'User registered successfully!'
+        ], 201);
     }
 
     // Login user
@@ -34,6 +39,7 @@ class AuthController extends Controller
             return response()->json([
                 'access_token' => $token,
                 'token_type' => 'Bearer',
+                'user' => $user
             ], 200);
         }
 
